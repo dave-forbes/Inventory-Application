a23f697f4b85800b06e8e3ff346996e9ca64f0d6
David Forbes <davidforbesi@hotmail.com>
2024-01-15 16:15:46 +0000

    added middleware to load static image assets in public folder

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use("/images", express.static(path.join(__dirname, "public/images")));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
