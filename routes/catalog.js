4b04fd5169ec3b439aef613f6bd85dc1612b0669
David Forbes <davidforbesi@hotmail.com>
2024-01-22 13:38:18 +0000

    modified catalog routes from copied over versions

--- routes/catalog.js
+++ routes/catalog.js
@@ -15,118 +15,118 @@ router.get("/", record_controller.index);
 // GET request for creating a Book. NOTE This must come before routes that display Book (uses id).
 router.get("/record/create", record_controller.record_create_get);
 
-// POST request for creating Book.
-router.post("/record/create", record_controller.record_create_post);
+// // POST request for creating Book.
+// router.post("/record/create", record_controller.record_create_post);
 
-// GET request to delete Book.
-router.get("/record/:id/delete", record_controller.record_delete_get);
+// // GET request to delete Book.
+// router.get("/record/:id/delete", record_controller.record_delete_get);
 
-// POST request to delete Book.
-router.post("/record/:id/delete", record_controller.record_delete_post);
+// // POST request to delete Book.
+// router.post("/record/:id/delete", record_controller.record_delete_post);
 
-// GET request to update Book.
-router.get("/record/:id/update", record_controller.record_update_get);
+// // GET request to update Book.
+// router.get("/record/:id/update", record_controller.record_update_get);
 
-// POST request to update record.
-router.post("/record/:id/update", record_controller.record_update_post);
+// // POST request to update record.
+// router.post("/record/:id/update", record_controller.record_update_post);
 
-// GET request for one record.
-router.get("/record/:id", record_controller.record_detail);
+// // GET request for one record.
+// router.get("/record/:id", record_controller.record_detail);
 
 // GET request for list of all record items.
 // router.get("/records", record_controller.record_list);
 
 /// AUTHOR ROUTES ///
 
-// GET request for creating Artist. NOTE This must come before route for id (i.e. display artist).
-router.get("/author/create", artist_controller.artist_create_get);
+// // GET request for creating Artist. NOTE This must come before route for id (i.e. display artist).
+// router.get("/author/create", artist_controller.artist_create_get);
 
-// POST request for creating artist.
-router.post("/artist/create", artist_controller.artist_create_post);
+// // POST request for creating artist.
+// router.post("/artist/create", artist_controller.artist_create_post);
 
-// GET request to delete artist.
-router.get("/artist/:id/delete", artist_controller.artist_delete_get);
+// // GET request to delete artist.
+// router.get("/artist/:id/delete", artist_controller.artist_delete_get);
 
-// POST request to delete artist.
-router.post("/artist/:id/delete", artist_controller.artist_delete_post);
+// // POST request to delete artist.
+// router.post("/artist/:id/delete", artist_controller.artist_delete_post);
 
-// GET request to update artist.
-router.get("/artist/:id/update", artist_controller.artist_update_get);
+// // GET request to update artist.
+// router.get("/artist/:id/update", artist_controller.artist_update_get);
 
-// POST request to update artist.
-router.post("/artist/:id/update", artist_controller.artist_update_post);
+// // POST request to update artist.
+// router.post("/artist/:id/update", artist_controller.artist_update_post);
 
-// GET request for one artist.
-router.get("/artist/:id", artist_controller.artist_detail);
+// // GET request for one artist.
+// router.get("/artist/:id", artist_controller.artist_detail);
 
-// GET request for list of all artists.
-router.get("/artists", artist_controller.artist_list);
+// // GET request for list of all artists.
+// router.get("/artists", artist_controller.artist_list);
 
-/// GENRE ROUTES ///
+// /// GENRE ROUTES ///
 
-// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
-router.get("/genre/create", genre_controller.genre_create_get);
+// // GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+// router.get("/genre/create", genre_controller.genre_create_get);
 
-//POST request for creating Genre.
-router.post("/genre/create", genre_controller.genre_create_post);
+// //POST request for creating Genre.
+// router.post("/genre/create", genre_controller.genre_create_post);
 
-// GET request to delete Genre.
-router.get("/genre/:id/delete", genre_controller.genre_delete_get);
+// // GET request to delete Genre.
+// router.get("/genre/:id/delete", genre_controller.genre_delete_get);
 
-// POST request to delete Genre.
-router.post("/genre/:id/delete", genre_controller.genre_delete_post);
+// // POST request to delete Genre.
+// router.post("/genre/:id/delete", genre_controller.genre_delete_post);
 
-// GET request to update Genre.
-router.get("/genre/:id/update", genre_controller.genre_update_get);
+// // GET request to update Genre.
+// router.get("/genre/:id/update", genre_controller.genre_update_get);
 
-// POST request to update Genre.
-router.post("/genre/:id/update", genre_controller.genre_update_post);
+// // POST request to update Genre.
+// router.post("/genre/:id/update", genre_controller.genre_update_post);
 
-// GET request for one Genre.
-router.get("/genre/:id", genre_controller.genre_detail);
+// // GET request for one Genre.
+// router.get("/genre/:id", genre_controller.genre_detail);
 
-// GET request for list of all Genre.
-router.get("/genres", genre_controller.genre_list);
+// // GET request for list of all Genre.
+// router.get("/genres", genre_controller.genre_list);
 
-/// BOOKINSTANCE ROUTES ///
+// /// BOOKINSTANCE ROUTES ///
 
-// GET request for creating a record copy. NOTE This must come before route that displays RecordCopy (uses id).
-router.get("/recordcopy/create", record_copy_controller.recordcopy_create_get);
+// // GET request for creating a record copy. NOTE This must come before route that displays RecordCopy (uses id).
+// router.get("/recordcopy/create", record_copy_controller.recordcopy_create_get);
 
-// POST request for creating recordcopy.
-router.post(
-  "/recordcopy/create",
-  record_copy_controller.recordcopy_create_post
-);
+// // POST request for creating recordcopy.
+// router.post(
+//   "/recordcopy/create",
+//   record_copy_controller.recordcopy_create_post
+// );
 
-// GET request to delete recordcopy.
-router.get(
-  "/recordcopy/:id/delete",
-  record_copy_controller.recordcopy_delete_get
-);
+// // GET request to delete recordcopy.
+// router.get(
+//   "/recordcopy/:id/delete",
+//   record_copy_controller.recordcopy_delete_get
+// );
 
-// POST request to delete recordcopy.
-router.post(
-  "/recordcopy/:id/delete",
-  record_copy_controller.recordcopy_delete_post
-);
+// // POST request to delete recordcopy.
+// router.post(
+//   "/recordcopy/:id/delete",
+//   record_copy_controller.recordcopy_delete_post
+// );
 
-// GET request to update recordcopy.
-router.get(
-  "/recordcopy/:id/update",
-  record_copy_controller.recordcopy_update_get
-);
+// // GET request to update recordcopy.
+// router.get(
+//   "/recordcopy/:id/update",
+//   record_copy_controller.recordcopy_update_get
+// );
 
-// POST request to update recordcopy.
-router.post(
-  "/recordcopy/:id/update",
-  record_copy_controller.recordcopy_update_post
-);
+// // POST request to update recordcopy.
+// router.post(
+//   "/recordcopy/:id/update",
+//   record_copy_controller.recordcopy_update_post
+// );
 
-// GET request for one recordcopy.
-router.get("/recordcopy/:id", record_copy_controller.recordcopy_detail);
+// // GET request for one recordcopy.
+// router.get("/recordcopy/:id", record_copy_controller.recordcopy_detail);
 
-// GET request for list of all recordcopy.
-router.get("/recordcopies", record_copy_controller.recordcopy_list);
+// // GET request for list of all recordcopy.
+// router.get("/recordcopies", record_copy_controller.recordcopy_list);
 
 module.exports = router;
