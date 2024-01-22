15e142f7cb5f679af34616beaef51e331d63923a
David Forbes <davidforbesi@hotmail.com>
2024-01-22 16:34:40 +0000

    new view and controller handler for genre create GET

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -1,42 +1,42 @@
 const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
 
-// Display list of all Genre.
-exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
-});
+// // Display list of all Genre.
+// exports.genre_list = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre list");
+// });
 
-// Display detail page for a specific Genre.
-exports.genre_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
-});
+// // Display detail page for a specific Genre.
+// exports.genre_detail = asyncHandler(async (req, res, next) => {
+//   res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`);
+// });
 
 // Display Genre create form on GET.
 exports.genre_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create GET");
+  res.render("genre_create");
 });
 
-// Handle Genre create on POST.
-exports.genre_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre create POST");
-});
+// // Handle Genre create on POST.
+// exports.genre_create_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre create POST");
+// });
 
-// Display Genre delete form on GET.
-exports.genre_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete GET");
-});
+// // Display Genre delete form on GET.
+// exports.genre_delete_get = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre delete GET");
+// });
 
-// Handle Genre delete on POST.
-exports.genre_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre delete POST");
-});
+// // Handle Genre delete on POST.
+// exports.genre_delete_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre delete POST");
+// });
 
-// Display Genre update form on GET.
-exports.genre_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update GET");
-});
+// // Display Genre update form on GET.
+// exports.genre_update_get = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre update GET");
+// });
 
-// Handle Genre update on POST.
-exports.genre_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre update POST");
-});
+// // Handle Genre update on POST.
+// exports.genre_update_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: Genre update POST");
+// });
