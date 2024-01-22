9136dd4ffaffc03a855a7d67023f0d9d70d72a68
David Forbes <davidforbesi@hotmail.com>
2024-01-22 16:34:55 +0000

    new route for genre and artist create GET

--- routes/catalog.js
+++ routes/catalog.js
@@ -38,8 +38,8 @@ router.get("/record/create", record_controller.record_create_get);
 
 /// AUTHOR ROUTES ///
 
-// // GET request for creating Artist. NOTE This must come before route for id (i.e. display artist).
-// router.get("/author/create", artist_controller.artist_create_get);
+// GET request for creating Artist. NOTE This must come before route for id (i.e. display artist).
+router.get("/artist/create", artist_controller.artist_create_get);
 
 // // POST request for creating artist.
 // router.post("/artist/create", artist_controller.artist_create_post);
@@ -64,8 +64,8 @@ router.get("/record/create", record_controller.record_create_get);
 
 // /// GENRE ROUTES ///
 
-// // GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
-// router.get("/genre/create", genre_controller.genre_create_get);
+// GET request for creating a Genre. NOTE This must come before route that displays Genre (uses id).
+router.get("/genre/create", genre_controller.genre_create_get);
 
 // //POST request for creating Genre.
 // router.post("/genre/create", genre_controller.genre_create_post);
