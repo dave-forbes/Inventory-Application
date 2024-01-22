ed2c8deca3fce140f4f45083febe586d583d9311
David Forbes <davidforbesi@hotmail.com>
2024-01-22 11:46:36 +0000

    add new handler for record create, deleted other copied over handlers

--- controllers/recordController.js
+++ controllers/recordController.js
@@ -1,10 +1,6 @@
 const Record = require("../models/record");
 const asyncHandler = require("express-async-handler");
 
-// exports.index = asyncHandler(async (req, res, next) => {
-//   res.render("index.pug");
-// });
-
 // Display list of all records.
 exports.index = asyncHandler(async (req, res, next) => {
   const allRecords = await Record.find()
@@ -17,42 +13,10 @@ exports.index = asyncHandler(async (req, res, next) => {
   res.render("index", { record_list: allRecords });
 });
 
-// Display list of all books.
-exports.book_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book list");
-});
-
-// Display detail page for a specific book.
-exports.book_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: Book detail: ${req.params.id}`);
-});
-
-// Display book create form on GET.
-exports.book_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create GET");
-});
-
-// Handle book create on POST.
-exports.book_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book create POST");
-});
-
-// Display book delete form on GET.
-exports.book_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete GET");
-});
-
-// Handle book delete on POST.
-exports.book_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book delete POST");
-});
-
-// Display book update form on GET.
-exports.book_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book update GET");
-});
-
-// Handle book update on POST.
-exports.book_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Book update POST");
+// Display record create form on GET.
+exports.record_create_get = asyncHandler(async (req, res, next) => {
+  res.render("record_create", {
+    artists: ["artist1", "artist2", "artist3"],
+    formats: ["format1", "format2"],
+  });
 });
