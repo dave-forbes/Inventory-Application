86c72bf2a6c41a93acb340015fb3ceffd6f84765
David Forbes <davidforbesi@hotmail.com>
2024-01-22 16:19:06 +0000

    implemented record_create_get

--- controllers/recordCopyController.js
+++ controllers/recordCopyController.js
@@ -1,42 +1,47 @@
 const RecordCopy = require("../models/recordCopy");
 const asyncHandler = require("express-async-handler");
+const Record = require("../models/record");
 
-// Display list of all RecordCopies.
-exports.recordcopy_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy list");
-});
+// // Display list of all RecordCopies.
+// exports.recordcopy_list = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy list");
+// });
 
-// Display detail page for a specific RecordCopy.
-exports.recordcopy_detail = asyncHandler(async (req, res, next) => {
-  res.send(`NOT IMPLEMENTED: RecordCopy detail: ${req.params.id}`);
-});
+// // Display detail page for a specific RecordCopy.
+// exports.recordcopy_detail = asyncHandler(async (req, res, next) => {
+//   res.send(`NOT IMPLEMENTED: RecordCopy detail: ${req.params.id}`);
+// });
 
 // Display RecordCopy create form on GET.
 exports.recordcopy_create_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy create GET");
+  const allRecords = await Record.find()
+    .populate("artist")
+    .populate("genre")
+    .exec();
+  res.render("recordcopy_create", { record_list: allRecords });
 });
 
-// Handle RecordCopy create on POST.
-exports.recordcopy_create_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy create POST");
-});
+// // Handle RecordCopy create on POST.
+// exports.recordcopy_create_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy create POST");
+// });
 
-// Display RecordCopy delete form on GET.
-exports.recordcopy_delete_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy delete GET");
-});
+// // Display RecordCopy delete form on GET.
+// exports.recordcopy_delete_get = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy delete GET");
+// });
 
-// Handle RecordCopy delete on POST.
-exports.recordcopy_delete_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy delete POST");
-});
+// // Handle RecordCopy delete on POST.
+// exports.recordcopy_delete_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy delete POST");
+// });
 
-// Display RecordCopy update form on GET.
-exports.recordcopy_update_get = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy update GET");
-});
+// // Display RecordCopy update form on GET.
+// exports.recordcopy_update_get = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy update GET");
+// });
 
-// Handle bookinstance update on POST.
-exports.bookinstance_update_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: RecordCopy update POST");
-});
+// // Handle RecordCopy update on POST.
+// exports.recordcopy_update_post = asyncHandler(async (req, res, next) => {
+//   res.send("NOT IMPLEMENTED: RecordCopy update POST");
+// });
