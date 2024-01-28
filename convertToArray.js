96c5906b35b7a4e89d13273c8119631f85b97a2e
David Forbes <davidforbesi@hotmail.com>
2024-01-28 10:18:12 +0000

    removed while loop

--- convertToArray.js
+++ convertToArray.js
@@ -1,8 +1,5 @@
 function convertToArray(string) {
   return string.split(",").map((item) => {
-    while (!/[a-zA-Z]/.test(item[0])) {
-      item = item.slice(1);
-    }
     return item.trim();
   });
 }
