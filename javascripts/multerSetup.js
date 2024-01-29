a601760e373feeb04736cd1d893e7ea7bed8edce
David Forbes <davidforbesi@hotmail.com>
2024-01-29 15:30:43 +0000

    update paths after moving file to different dir

--- javascripts/multerSetup.js
+++ javascripts/multerSetup.js
@@ -2,9 +2,12 @@ const path = require("path");
 const multer = require("multer");
 
 // Set up multer storage and specify the destination for uploaded files
+
+const parentDir = path.resolve(__dirname, "..");
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, path.join(__dirname, "public/images"));
+    cb(null, path.join(parentDir, "public/images"));
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
